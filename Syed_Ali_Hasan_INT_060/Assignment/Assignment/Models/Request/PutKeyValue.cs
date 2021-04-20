d840b87a5ab9b419d37edbc5a051bf5b0ca67538
Syed Ali Hasan <syed.hasan@cybergindia.local>
2021-04-20 20:32:09 +0530

    .Net Assignment. Everything done except PUT Method

---
+++ Syed_Ali_Hasan_INT_060/Assignment/Assignment/Models/Request/PutKeyValue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment.Models.Request
+{
+    public class PutKeyValueString
+    {
+        public string key { get; set; }
+        public string value { get; set; }
+    }
+
+    public class PutKeyValueInt
+    {
+        public int key { get; set; }
+        public int value { get; set; }
+    }
+}
